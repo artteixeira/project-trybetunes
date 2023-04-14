33f188b17f4d7c0e742f5b2d3ceee7fa800eebbf
Arthur Teixeira <arthursony1@hotmail.com>
2023-04-13 21:08:14 -0300

    requisito 6

---
+++ src/components/AlbumCard.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class AlbumCard extends Component {
+  render() {
+    const { artistName, albumName, albumThumb, linkAlbum } = this.props;
+    return (
+      <Link data-testid={ `link-to-album-${linkAlbum}` } to={ `/album/${linkAlbum}` }>
+        <div>
+          <img src={ albumThumb } alt={ albumName } />
+          <p>{albumName}</p>
+          <p>{artistName}</p>
+        </div>
+      </Link>
+    );
+  }
+}
+
+AlbumCard.propTypes = {
+  artistName: PropTypes.string.isRequired,
+  albumName: PropTypes.string.isRequired,
+  albumThumb: PropTypes.string.isRequired,
+  linkAlbum: PropTypes.string.isRequired,
+};
+
+export default AlbumCard;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from '../components/Loading';
+import AlbumCard from '../components/AlbumCard';
 
 class Search extends Component {
   state = {
     artist: '',
     disableBtn: true,
+    albums: [],
+    loading: false,
+    oldArtist: '',
+    verify: false,
   };
 
   handleChange = ({ target }) => {
@@ -25,26 +32,64 @@ class Search extends Component {
     });
   };
 
+  albumVerify = () => {
+    const { albums } = this.state;
+    const val = albums.length > 1;
+    this.setState({
+      verify: !val,
+    });
+  };
+
+  handleClick = async () => {
+    const { artist } = this.state;
+    this.setState({
+      loading: true,
+    });
+    const albums = await searchAlbumsAPI(artist);
+    this.setState({
+      albums,
+      oldArtist: artist,
+      artist: '',
+      loading: false,
+    }, this.albumVerify);
+  };
+
   render() {
-    const { artist, disableBtn } = this.state;
+    const { artist, disableBtn, loading, albums, oldArtist, verify } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        { loading ? <Loading /> : (
+          <section>
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="artist"
+              id="artist"
+              value={ artist }
+              onChange={ this.handleChange }
+            />
+            <button
+              data-testid="search-artist-button"
+              disabled={ disableBtn }
+              onClick={ this.handleClick }
+            >
+              Pesquisar
+            </button>
+          </section>)}
         <main>
-          <input
-            data-testid="search-artist-input"
-            type="text"
-            name="artist"
-            id="artist"
-            value={ artist }
-            onChange={ this.handleChange }
-          />
-          <button
-            data-testid="search-artist-button"
-            disabled={ disableBtn }
-          >
-            Pesquisar
-          </button>
+          <p>{ `Resultado de álbuns de: ${oldArtist}`}</p>
+          { verify
+            ? <p>Nenhum álbum foi encontrado</p>
+            : (albums
+              .map((element, index) => (
+                <AlbumCard
+                  key={ index }
+                  artistName={ element.artistName }
+                  albumName={ element.collectionName }
+                  albumThumb={ element.artworkUrl100 }
+                  linkAlbum={ element.collectionId }
+                />)))}
         </main>
       </div>
     );
